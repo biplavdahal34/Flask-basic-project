3051017ce9038f2523fe0a385ec6e921bf55bd87
biplavdahal34 <dahalbiplav58@gmail.com>
2026-08-21 21:37:06 +0545

    top bar made and posts are bordered

--- flask_project.py
+++ flask_project.py
@@ -1,18 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
 dummy_post = [{
     "name" : "ricky",
     "age"  : "24",
-    "title": "my first post yall",
+    "title": "First Post",
     "content": "hello, i am rick. nice to meet you all",
     "date" : "May 20, 2026",
 },
 {
     "name" : "morty",
     "age"  : "16",
-    "title": "my post yall",
+    "title": "New post, Hello",
     "content": "hello, i am morty. nice to meet you all",
     "date" : "june 17, 2026",
 },]

e908a19c8eb8b4d4ec8851b362be31bc821e43f4
biplavdahal34 <dahalbiplav58@gmail.com>
2026-08-22 23:57:31 +0545

    major database being made

--- flask_project.py
+++ flask_project.py
@@ -1,8 +1,33 @@
+from datetime import datetime
 from flask import Flask,render_template,url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import Registration, Login
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "398ro83reufheuf838ue438uff3498eyf"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique = True, nullable = False)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    image = db.Column(db.String(20), nullable = False, default= "default.jpeg")
+    password = db.Column(db.String(60), nullable = False)
+    posts = db.relationship('Post', backref="author", lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 dummy_post = [{
     "name" : "ricky",
@@ -19,9 +44,9 @@ dummy_post = [{
     "date" : "june 17, 2026",
 },]
 
-@app.route("/support")
-def support():
-    return render_template("support.html", title = "Support")
+@app.route("/about")
+def about():
+    return render_template("about.html", title = "About")
 
 @app.route("/home")
 @app.route("/")
@@ -36,9 +61,15 @@ def register():
         return redirect(url_for("home"))
     return render_template("register.html", title = "Register", form = form)
 
-@app.route("/login")
+@app.route("/login",methods=['GET','POST'])
 def login():
     form = Login()
+    if form.validate_on_submit():
+        if form.email.data == "biplav123@gmail.com" and form.password.data == "password":
+            flash("Welcome", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Failed. Please Try Again", "danger")
     return render_template("login.html", title = "Login", form = form)
 
 if __name__ == "__main__":

3fa49cd853c2d4037b1530f32640bb093dafaf8a
biplavdahal34 <dahalbiplav58@gmail.com>
2026-08-22 19:46:14 +0545

    added register and login pages

--- flask_project.py
+++ flask_project.py
@@ -1,6 +1,8 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template,url_for, flash, redirect
+from forms import Registration, Login
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "398ro83reufheuf838ue438uff3498eyf"
 
 dummy_post = [{
     "name" : "ricky",
@@ -18,13 +20,26 @@ dummy_post = [{
 },]
 
 @app.route("/support")
-def hello_world():
+def support():
     return render_template("support.html", title = "Support")
 
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("home.html", posts = dummy_post)
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = Registration()
+    if form.validate_on_submit():
+        flash(f'Account Created For {form.username.data}!','success')
+        return redirect(url_for("home"))
+    return render_template("register.html", title = "Register", form = form)
+
+@app.route("/login")
+def login():
+    form = Login()
+    return render_template("login.html", title = "Login", form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)

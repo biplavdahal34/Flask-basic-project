37d4a5346059b308b81679eca41ad425c220871d
biplavdahal34 <dahalbiplav58@gmail.com>
2026-08-24 23:53:14 +0545

    added account page and fixed malicious access

--- flask_project/routes.py
+++ flask_project/routes.py
@@ -2,7 +2,7 @@ from flask import render_template,url_for, flash, redirect
 from flask_project import app,db,bcrypt
 from flask_project.models import User, Post
 from flask_project.forms import Registration, Login
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, logout_user, login_required
 dummy_post = [{
     "name" : "ricky",
     "age"  : "24",
@@ -54,3 +54,13 @@ def login():
         else:
             flash("Login Failed. Please Try Again", "danger")
     return render_template("login.html", title = "Login", form = form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title = "Account")
